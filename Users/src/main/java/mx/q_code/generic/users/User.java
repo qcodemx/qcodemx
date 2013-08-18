85e9fb7f3a5e40ba40d8af31fd5f20edb166687f
fernando <fercho@fercho>
2013-08-17 20:56:09 -0500

    Commit Test GitHub

---
+++ Users/src/main/java/mx/q_code/generic/users/User.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.q_code.generic.users;
+
+/**
+ *
+ * @author fercho
+ */
+public class User {
+    String name;
+}
